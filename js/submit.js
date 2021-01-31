628714554ac7afaaf42219eb6e624811db5ca0ec
Volodymyr Ryba <vovaryba@gmail.com>
2021-01-31 17:08:26 +0200

    Подправил скрипт для того, чтобы он срабатывал для формы модального окна

--- js/submit.js
+++ js/submit.js
@@ -11,3 +11,16 @@
 			e.currentTarget.reset();
 		});
 })();
+(() => {
+	document
+		.querySelector('.js-speaker-form1')
+		.addEventListener('submit', e => {
+			e.preventDefault();
+
+			new FormData(e.currentTarget).forEach((value, name) =>
+				console.log(`${name}: ${value}`),
+			);
+
+			e.currentTarget.reset();
+		});
+})();
